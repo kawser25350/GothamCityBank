da1e412fdef38c8f0ea754ecb4762c37db1e5121
Kawser Ahmmed <mkshuvo25350@gmail.com>
2026-09-05 00:18:30 +0600

    secrect key and deployment test-2

--- bank_managment/settings.py
+++ bank_managment/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=+xe!wr05!uj4ly2zaawnx5ful6c+8(srfpl43+npibty!ie-z"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 
 # Application definition
